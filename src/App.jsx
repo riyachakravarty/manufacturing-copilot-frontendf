a8087acb67cee360c944a0678899bc8df33f8cae
riyachakravarty <riya.chakravarty2010@gmail.com>
2025-07-31 15:34:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Plot from "react-plotly.js";
-import Select from "react-select";
+#import Select from "react-select";
 
 const App = () => {
   const [columns, setColumns] = useState([]);
